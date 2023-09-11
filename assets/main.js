f62b80a8a66da32fca7eaffa4cc10aa9c4805f49
Ogunaike Matthew Folawe <125424571+Fivestones-Tech@users.noreply.github.com>
2023-09-11 02:18:37 +0100

    adding email.js to the email section

--- assets/main.js
+++ assets/main.js
@@ -19,15 +19,31 @@ if(navClose) {
     })
 }
 
+
+
 // ================REMOVE MENU MOBILE=============
-const navLink = document.querySelectorAll('.nav__link')
+// const navLink = document.querySelectorAll('.nav__link')
+
+// const linkAction = () => {
+//     // const navMenu = document.getElementById('nav-menu')
+//     const navMenu = document.getElementById('#nav-menu')
+//     // when we click on each nav link we remove the show-menu 
+//     navMenu.classList.remove('show-menu')
+// }
+// navLink.forEach(n => n.addEventListener('click', linkAction))
+const navLink = document.querySelectorAll('.nav__link');
 
 const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu')
+    const navMenu = document.querySelector('#nav--menu'); // Updated to match the ID
     // when we click on each nav link we remove the show-menu 
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+
+    navMenu.classList.remove('show-menu');
+};
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+
 
 
 // =============SWIPER POJECTS=============
@@ -80,40 +96,12 @@ const sendEmail = (e) => {
         contactMessage.classList.add('color-red');
 
         // show message
-        contactMessage.textContent = 'Write all the input fields'
+        contactMessage.textContent = 'Please fill in all the input fields. ⚠️'
     }
 
 };
 
 contactForm.addEventListener('submit', sendEmail);
-
-// const contactForm = document.getElementById('contact-form');
-// const contactName = document.getElementById('contact-name');
-// const contactEmail = document.getElementById('contact-email');
-// const contactProject = document.getElementById('contact-project');
-// const contactMessage = document.getElementById('contact-message');
-
-// const sendEmail = (e) => {
-//     e.preventDefault();
-
-//     // check if the fields have a value
-//     if (contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
-//         // add and remove color
-//         contactMessage.classList.remove('color-blue');
-//         contactMessage.classList.add('color-red');
-
-//         // show message
-//         contactMessage.textContent = 'Please fill in all the input fields. ⚠️';
-//     }
-// };
-
-// contactForm.addEventListener('submit', sendEmail);
-
-
-
-
-
-
 
 
 
