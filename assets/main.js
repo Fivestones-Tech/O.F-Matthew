ec1a6de0fdebb01738db259a6c5ed18ab7d2dfee
Ogunaike Matthew Folawe <125424571+Fivestones-Tech@users.noreply.github.com>
2023-09-13 18:34:21 +0100

    style

--- assets/main.js
+++ assets/main.js
@@ -41,7 +41,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 // This is for typed script js
 const typed = new Typed('.multiple-text',{
-    strings: ['Software Engineer','Frontend Developer','Code Tutor','YouTuber','Web/U.I Designer','Your Tech Coach & Mentor','Content Creator','Multi-Entrepreneur'],
+    strings: ['Software Engineer','Frontend Developer','Code Tutor','YouTuber','Web/U.I Designer','Tech Coach', 'Mentor','Content Creator','Multi-Entrepreneur'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
@@ -189,35 +189,6 @@ window.addEventListener('scroll', scrollUp)
 
 
 // =============DARK LIGHT THEME=================
-// const themeButton = document.getElementById('theme-button')
-// const darkTheme = 'dark-theme'
-// const iconTheme = 'ri-sun-line'
-
-// // previously selected topic (if user selected)
-// const selectedTheme = localStorage.getItem('selected-theme')
-// const selectedIcon = localStorage.getItem('selected-icon')
-
-// // we obtain the current theme the interface has by validating the dark-theme classs
-// const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-// const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'                                                                                          n' : 'ri sun-line'
-
-// //  we validate if the user previously chose a topic 
-// if (selectedTheme) {
-//     // if the validation is fulfilled, we ask the issue was to know if we activated or deactivated the dak 
-//     document.body.classList[selectedTheme === 'dark' ? : 'remove'](darkTheme)
-//     themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
-// }
-
-// // Activate / desactivate the theme manually with the button 
-// themeButton.addEventListener('click', () => {
-//             // add or remove the dark / icon theme 
-//             document.body.classList.toggle(darkTheme)
-//             themeButton.classList.toggle(iconTheme)
-//             // we save the theme and the current icon that the user chose 
-//             localStorage.setItem('selected-theme', getCurrentTheme())
-//             localStorage.setItem('selected-icon', getCurrentIcon())
-// })
-
 const themeButton = document.getElementById('theme-button');
 const darkTheme = 'dark-theme';
 const iconTheme = 'ri-sun-line';
